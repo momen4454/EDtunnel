5d770f551da29646e3fdb4b4b76698612f0dad45
momen4454 <78962022+momen4454@users.noreply.github.com>
2024-11-14 20:59:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '432c2d84-ae6d-4249-b2f4-1a2ebc91ef60';
 
 const proxyIPs = ['cdn.xn--b6gac.eu.org:443', 'cdn-all.xn--b6gac.eu.org:443'];
 
